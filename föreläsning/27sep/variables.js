5806d094fc3aeab9fa2d207bef0b5bfddd512bfa
samthe0908 <stheerawat0908@gmail.com>
2023-09-28 11:12:08 +0200

    create bloggmenu

---
+++ föreläsning/27sep/variables.js
@@ -0,0 +1,13 @@
+let name = "Samantha";
+let other = "Tina";
+console.log(name === other);
+
+let x = 1;
+let y = 2;
+console.log(y > x)
+console.log(x < y)
+x = x + x;
+console.log(x!==y)
+
+    
+
