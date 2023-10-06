b8e585c43b5d12784b7a2d5f8b1390bdb633100f
samthe0908 <stheerawat0908@gmail.com>
2023-10-06 09:31:29 +0200

    Create ifelse.js

---
+++ föreläsning/28sep/ifelse.js
@@ -0,0 +1,39 @@
+/*const hour = new Date().getHours();
+console.log(hour)
+
+if (hour>18 || hour < 6) {
+    console.log("det är mörk")
+    
+} else {
+    console.log ("det är ljus")
+}*/
+
+/*const age = 22;
+if (age < 15) {
+    console.log("saft")  
+} else if (age >= 15 && age < 18) {
+    console.log("kan smaka")
+} else if (age >= 18 && age <= 21)
+    console.log("öl")
+else {
+    console.log("feel free")
+}*/
+ 
+*const myAge = 30;
+const myName = "Samantha";
+
+let friendsAge = 22;
+let friendsName = "Tina";
+
+if (myAge > friendsAge) {
+    console.log(myName)
+
+} else if (myAge < friendsAge) {
+    console.log(friendsName)
+
+} else {
+    console.log("???")
+}
+    
+
+
